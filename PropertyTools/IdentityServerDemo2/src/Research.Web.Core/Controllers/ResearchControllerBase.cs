8050b2ae6d3973ee832fce96132d217c1f90c602
帕瓦罗地瓜PowerDG <1049365046@qq.com>
2019-07-14 20:43:52 +0800

    Update ResearchControllerBase.cs

--- PropertyTools/IdentityServerDemo2/src/Research.Web.Core/Controllers/ResearchControllerBase.cs
+++ PropertyTools/IdentityServerDemo2/src/Research.Web.Core/Controllers/ResearchControllerBase.cs
@@ -1,11 +1,14 @@
 ﻿using Abp.AspNetCore.Mvc.Controllers;
 using Abp.IdentityFramework;
 using Microsoft.AspNetCore.Identity;
+using Research.Extensions;
 
 namespace Research.Controllers
 {
     public abstract class ResearchControllerBase: AbpController
     {
+        //隐藏父类的AbpSession
+        //public new IAbpSessionExtension AbpSession { get; set; }
         protected ResearchControllerBase()
         {
             LocalizationSourceName = ResearchConsts.LocalizationSourceName;
